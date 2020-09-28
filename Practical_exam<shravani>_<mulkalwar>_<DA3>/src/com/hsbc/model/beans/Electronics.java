8bfdb7bdbfd92095f07da80f0355f9ce491cb2cf
shravani211998 <71273966+shravani211998@users.noreply.github.com>
2020-09-28 09:26:42 +0530

    Create Electronics.java

---
+++ Practical_exam<shravani>_<mulkalwar>_<DA3>/src/com/hsbc/model/beans/Electronics.java
@@ -0,0 +1,27 @@
+package com.hsbc.model.beans;
+
+public class Electronics extends Product {
+	
+	
+	
+	int warranty;
+	
+	public Electronics(int itemCode, String itemName, double unitprize, int quantity,int warranty) {
+		
+		super(itemCode,itemName,unitprize,quantity);
+		this.warranty = warranty;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
